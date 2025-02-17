4ffd8f2f52824c53aa1c9c39e04b4b9f1b3dd80f
yisuscena <2230224@upv.edu.mx>
2025-02-17 07:00:20 -0600

    Falta determinar

--- letter.py
+++ letter.py
@@ -2,3 +2,10 @@ class Letter:
     def __init__(self, contour, center):
         self.contour = contour
         self.center = center
+        
+        
+    def getCenter(self):
+        return self.center
+        
+    def getContour(self):
+        return self.contour
--- pruebas.py
+++ pruebas.py
@@ -1,5 +1,16 @@
+"""
+Las sopas de letras de este programa son ejemplos
+proporcionados por:
+
+https://www.superteacherworksheets.com/generator-word-search.html
+
+
+"""
+
+
 import cv2
 import numpy as np
+import math
 from letter import Letter
 
 def correct_perspective(img):
@@ -39,11 +50,11 @@ def process_img(img):
     # Prueba adaptiveThreshold en lugar de Canny
     img_thresh = cv2.adaptiveThreshold(
         img_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
-        cv2.THRESH_BINARY_INV, 11, 2
+        cv2.THRESH_BINARY_INV, 11, 9
     )
 
     # Aplicar dilatación con kernel más pequeño
-    kernel = np.ones((2, 2), np.uint8)
+    kernel = np.ones((1, 1), np.uint8)
     img_dilate = cv2.dilate(img_thresh, kernel, iterations=1)
     
     return img_dilate
@@ -66,11 +77,28 @@ def get_centers(img):
         if hierarchy[0][i][3] != -1:  # Verifica si el contorno tiene un padre (hijo de otro)
             continue  # Ignora los contornos internos como el hueco de la "O"
 
-        if cv2.contourArea(cnt) > 130 and (max_contour is None or not np.array_equal(cnt, max_contour)):
+        if cv2.contourArea(cnt) > 20:
             center = get_centeroid(cnt)
             if center:
                 letter = Letter(cnt,center)
                 figures.append(letter)
+                yield center
+                
+def getCentersDataset(img):
+    global datasetCenters
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    max_contour = max(contours, key=cv2.contourArea) if contours else None
+    
+    for i, cnt in enumerate(contours):
+        if hierarchy[0][i][3] != -1:  # Verifica si el contorno tiene un padre (hijo de otro)
+            continue  # Ignora los contornos internos como el hueco de la "O"
+
+        if cv2.contourArea(cnt) > 20:
+            center = get_centeroid(cnt)
+            if center:
+                letter = Letter(cnt,center)
+                datasetCenters.append(letter)
                 yield center
     
 
@@ -81,56 +109,141 @@ def get_rows(centers, row_amt, row_h):
         f = centers[:, 1] - d * i
         a = centers[(f < d) & (f > 0)]
         yield a[a.argsort(0)[:, 0]]
+        
+def prepareDataset():
+    global abc, datasetCenters
+    abc = cv2.imread("dataset.png")
+    abc = cv2.resize(abc, (800,800))
+    abc = correct_perspective(abc)
+    abc = correct_perspective(abc)
+    abc_processed = process_img(abc)
+    datasetCentersList = list(getCentersDataset(abc_processed))
+    #contours, hierarchy = cv2.findContours(abc_processed, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    h, w, c = abc.shape
+    count = 0
+    orderDataset = []
+    for row in get_rows(datasetCentersList, 9, w):
+        for x, y in row:
+            for letter in datasetCenters:
+                if(letter.getCenter() == (x,y)):
+                    orderDataset.append(letter)
+                    break
+            count += 1
+            cv2.circle(abc, (x, y), 10, (0, 0, 255), -1)  
+            cv2.putText(abc, str(count), (x - 10, y + 5), 1, cv2.FONT_HERSHEY_PLAIN, (0, 255, 255), 2)
+    datasetCenters = orderDataset
+    cv2.imshow("Dataset", abc)
+    
+        
+    
+def prepareImg():
+    global img, figures
+    img = cv2.imread("prueba2.jpg")
+    img = cv2.resize(img, (800,800))
+    img = correct_perspective(img)
+    img = correct_perspective(img)
+    img_processed = process_img(img)
+    centersList = list(get_centers(img_processed))
+    h, w, c = img.shape
+    count = 0
+    orderFigures = []
+    for row in get_rows(centersList, 8, w):
+        #cv2.polylines(img, [row], False, (255, 0, 255), 2)
+        for x, y in row:
+            for letter in figures:
+                if(letter.getCenter() == (x,y)):
+                    orderFigures.append(letter)
+                    break
+            count += 1
+            cv2.circle(img, (x, y), 10, (0, 0, 255), -1)  
+            cv2.putText(img, str(count), (x - 10, y + 5), 1, cv2.FONT_HERSHEY_PLAIN, (0, 255, 255), 2)
+    figures = orderFigures
+    
+def prepareImg2():
+    global img2
+    img2 = cv2.imread("prueba2.jpg")
+    img2 = cv2.resize(img2, (800,800))
+    img2 = correct_perspective(img2)
+    img2 = correct_perspective(img2)
+    
+    
+        
+    
+sentence = "TOYS"
 
-sentence = "coco"
+datasetCenters = []
 figures = []
+similarity = {
+    'A':0.30,
+    'B':0.30,
+    'C':5.0,
+    'D':0.10,
+    #La letra E funciona muy mal agarra practicamente todas las letras
+    'E':71,
+    #Esta tambien funciona horrible
+    'F':87,
+    #Esta es preocupante por que agarra E y F antes que la G
+    'G':68,
+    #Esta agarra G
+    'H':5,
+    #Esta agarra muchas letras
+    'I':10,
+    #Este agarra I
+    'J':4.5,
+    'K':200,
+    #Esto Agarra I
+    'L':5.8,
+    #Esto agarra N
+    'M':1.5,
+    #Esto agarra la M
+    'N':1.4,
+    #Esto agarra la Q
+    'O':0.03,
+    'P':0.30,
+    #Es una gran ventaja esto
+    'Q':0.02,
+    'R':0.30,
+    #Puta letra de mierda te odio
+    'S':390,
+    #Agarra Muchas letras
+    'T':32,
+    #Agarra Muchas letras
+    'U':75.8,
+    #No hay ninguna V
+    'V':0.10,
+    #Agarra N y M
+    'W':1.1,
+    #No hay ninguna X
+    'X':0.10,
+    #CHTM
+    'Y':1350,
+    #Agarra S y otras cosas
+    'Z':9
+}
 
-abc = cv2.imread("abecedario.png")
-abc = process_img(abc)
+abc = None
+prepareDataset()
 
-# Carga la imagen y corrige perspectiva
-img = cv2.imread("prueba2.jpg")
-img = cv2.resize(img, (800, 800))
-img = correct_perspective(img)
-img = correct_perspective(img)
-# Opcional: Endereza si está torcida
-img2 = correct_perspective(img)
+img = None
+prepareImg()
 
-img_processed = process_img(img)
-centers = list(get_centers(img_processed))
-contours, hierarchy = cv2.findContours(img_processed, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-abc_contours, _ = cv2.findContours(abc, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-abc_contours = sorted(abc_contours, key=lambda c: cv2.boundingRect(c)[0])
+img2= None
+prepareImg2()
 
-h, w, c = img.shape
-count = 0
-
-letter = abc_contours[0]
+letA = datasetCenters[4].getContour()
+print(cv2.contourArea(letA))
 
 for c in figures:
-    if cv2.contourArea(c.contour) < 130:
-        continue
-    
-    similarity = cv2.matchShapes(letter,c.contour,cv2.CONTOURS_MATCH_I1, 0.0)
-    if similarity < 0.33:
-        hull = cv2.convexHull(c.contour)
+    similarity = cv2.matchShapes(letA,c.getContour(),cv2.CONTOURS_MATCH_I1, 0.0)
+    if similarity < 71 :
+        hull = cv2.convexHull(c.getContour())
         (x_center, y_center), radius = cv2.minEnclosingCircle(hull)
         center = (int(x_center), int(y_center))
         radius = int(radius)
-        cv2.circle(img2, center, radius, (0, 255, 0), 2)  
-    
+        cv2.circle(img2, center, radius, (0, 255, 0), 2) 
 
-for row in get_rows(centers, 8, h):
-    cv2.polylines(img, [row], False, (255, 0, 255), 2)
-    for x, y in row:
-        count += 1
-        cv2.circle(img, (x, y), 10, (0, 0, 255), -1)  
-        cv2.putText(img, str(count), (x - 10, y + 5), 1, cv2.FONT_HERSHEY_PLAIN, (0, 255, 255), 2)
 
-cv2.imshow("Ordered", img)
-cv2.imshow("Circulos", img2)
-cv2.imshow("Contornos", img_processed)
-cv2.imshow("Abecedario", abc)
-
+cv2.imshow("OrderImg", img)
+cv2.imshow("img2", img2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
