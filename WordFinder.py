0ac3a967d1d5130e90978d1f736143306f61f6b2
yisuscena <2230224@upv.edu.mx>
2025-02-17 23:08:32 -0600

    La quemaron toda

--- WordFinder.py
+++ WordFinder.py
@@ -4,14 +4,18 @@ proporcionados por:
 
 https://www.superteacherworksheets.com/generator-word-search.html
 
+Con la configuración de nivel Basico 12*8
+y la fuente de letras en Mayusculas
 
 """
 
 
 import cv2
 import numpy as np
-import math
 from letter import Letter
+import sys
+from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6.QtCore import QSize, pyqtSignal
 
 def correct_perspective(img):
     """ Corrige perspectiva sin girar la imagen """
@@ -133,13 +137,12 @@ def prepareDataset():
             cv2.circle(abc, (x, y), 10, (0, 0, 255), -1)  
             cv2.putText(abc, str(count), (x - 10, y + 5), 1, cv2.FONT_HERSHEY_PLAIN, (0, 255, 255), 2)
     datasetCenters = orderDataset
-    cv2.imshow("Dataset", abc)
     
         
     
 def prepareImg():
     global img, figures
-    img = cv2.imread("prueba2.jpg")
+    img = cv2.imread(ruta)
     img = cv2.resize(img, (800,800))
     img = correct_perspective(img)
     img = correct_perspective(img)
@@ -161,8 +164,8 @@ def prepareImg():
     figures = orderFigures
     
 def prepareImg2():
-    global img2
-    img2 = cv2.imread("prueba2.jpg")
+    global img2, ruta
+    img2 = cv2.imread(ruta)
     img2 = cv2.resize(img2, (800,800))
     img2 = correct_perspective(img2)
     img2 = correct_perspective(img2)
@@ -214,7 +217,132 @@ def finalResult():
             radius = int(radius)
             cv2.circle(img2, center, radius, (0, 255, 0), 2)  
     
-sentence = "BOY"
+
+class MiEtiqueta(QtWidgets.QLabel):
+    clicked = pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self.Lista = []
+        self.setStyleSheet("border: 1px solid black;")
+
+
+class Window(QtWidgets.QWidget):
+    def Metodo(self):
+        for i in self.viewer.Lista:
+            ii = tuple(int(x) for x in i)
+            self.OpenCV_image = cv2.circle(self.OpenCV_image, ii, 10, (255, 255, 0), 4)
+        self.ActualizarPixMap()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = self.screen().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(10, 10, 900, 600)
+        self.center()
+
+        self.viewer = MiEtiqueta()
+        self.viewer2 = MiEtiqueta()
+        self.viewer.clicked.connect(self.Metodo)
+
+        self.buttonOpen = QtWidgets.QPushButton("Open Image")
+        BUTTON_SIZE = QSize(200, 50)
+        self.buttonOpen.setMinimumSize(BUTTON_SIZE)
+        self.buttonOpen.clicked.connect(self.handleOpen)
+
+        self.textInput = QtWidgets.QLineEdit()
+        self.textInput.setMinimumSize(BUTTON_SIZE)
+
+        self.enterButton = QtWidgets.QPushButton("Buscar")
+        self.enterButton.setMinimumSize(BUTTON_SIZE)
+        self.enterButton.clicked.connect(self.handleTextInput)
+
+        self.guardarImagen = QtWidgets.QPushButton("Guardar")
+        self.guardarImagen.setMinimumSize(BUTTON_SIZE)
+        self.guardarImagen.clicked.connect(self.handleSaveFile)
+
+        layout = QtWidgets.QGridLayout(self)
+        layout.addWidget(self.buttonOpen, 0, 0, 1, 1)
+        layout.addWidget(self.guardarImagen, 0, 3, 1, 1)
+        layout.addWidget(self.textInput, 0, 1, 1, 1)
+        layout.addWidget(self.enterButton, 0, 2, 1, 1)
+        layout.addWidget(self.viewer, 1, 0, 1, 2)
+        layout.addWidget(self.viewer2, 1, 2, 1, 2)
+
+    def handleTextInput(self):
+        global sentence
+        text = self.textInput.text()
+        if text :
+            sentence = text
+            finalResult()
+            self.ActualizarImagen2()
+        
+
+    def handleSaveFile(self):
+        global img2
+        fileName, _ = QtWidgets.QFileDialog.getSaveFileName(self, "Save File", "", "Images(*.jpg *.png)")
+        if fileName:
+            cv2.imwrite(fileName, self.Second_image)
+
+    def handleOpen(self):
+        global ruta,figures,datasetCenters
+        
+        path = QtWidgets.QFileDialog.getOpenFileName(self, "Choose File", "./", "Images(*.jpg *.png)")[0]
+        if path:
+            self._path = path
+            ruta = self._path
+            prepareDataset()
+            prepareImg()
+            prepareImg2()
+            for c in figures:
+                """ print(str(count) + " " + str(cv2.contourArea(c.getContour())))
+                count += 1 """
+                for letABC in datasetCenters:
+                    similarity = cv2.matchShapes(letABC.getContour(),c.getContour(),cv2.CONTOURS_MATCH_I1, 0.0)
+                    if similarity < letABC.getSimilarityValue():
+                        c.setPosibleLetter(letABC.getLetter())
+            self.ActualizarImagen()
+
+    def ActualizarPixMap(self):
+        QImageTemp = QtGui.QImage(cv2.cvtColor(self.OpenCV_image, cv2.COLOR_BGR2RGB),
+                                  self.OpenCV_image.shape[1],
+                                  self.OpenCV_image.shape[0],
+                                  self.OpenCV_image.shape[1] * 3,
+                                  QtGui.QImage.Format.Format_RGB888)
+        pixmap = QtGui.QPixmap(QImageTemp)
+        self.viewer.setPixmap(pixmap)
+
+    def ActualizarImagen2(self):
+        global img2
+        self.Second_image = img2
+        tamano = (self.viewer2.size().width(), self.viewer2.size().height())
+        self.Second_image = cv2.resize(self.Second_image, tamano, interpolation=cv2.INTER_LINEAR)
+
+        QImageTemp2 = QtGui.QImage(cv2.cvtColor(self.Second_image, cv2.COLOR_BGR2RGB),
+                                   self.Second_image.shape[1],
+                                   self.Second_image.shape[0],
+                                   self.Second_image.shape[1] * 3,
+                                   QtGui.QImage.Format.Format_RGB888)
+        pixmap2 = QtGui.QPixmap(QImageTemp2)
+        self.viewer2.setPixmap(pixmap2)
+
+    def ActualizarImagen(self):
+        self.OpenCV_image = cv2.imread(self._path)
+        tamano = (self.viewer.size().width(), self.viewer.size().height())
+        self.OpenCV_image = cv2.resize(self.OpenCV_image, tamano, interpolation=cv2.INTER_LINEAR)
+        self.ActualizarPixMap()
+    
+    
+    
+ruta = ''  
+sentence = ""
+abc = None
+img = None
+img2= None
 
 datasetCenters = []
 figures = []
@@ -266,30 +394,12 @@ dataSimilarity = {
     'Z':9
 }
 
-abc = None
-prepareDataset()
+app = QtWidgets.QApplication(sys.argv)
+window = Window()
+window.setWindowTitle("Soup")
+window.show()
+sys.exit(app.exec())
 
-img = None
-prepareImg()
-
-img2= None
-prepareImg2()
 
 #letA = datasetCenters[4].getContour()
 #print(cv2.contourArea(letA))
-
-for c in figures:
-    """ print(str(count) + " " + str(cv2.contourArea(c.getContour())))
-    count += 1 """
-    for letABC in datasetCenters:
-        similarity = cv2.matchShapes(letABC.getContour(),c.getContour(),cv2.CONTOURS_MATCH_I1, 0.0)
-        if similarity < letABC.getSimilarityValue():
-            c.setPosibleLetter(letABC.getLetter())
-            
-finalResult()
-
-
-cv2.imshow("OrderImg", img)
-cv2.imshow("img2", img2)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
